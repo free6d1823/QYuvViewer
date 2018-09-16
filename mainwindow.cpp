e13a25e8a649d1d7290acab58124a2c53759a293
jammy <chengjyhchang@gmail.com>
2018-09-16 23:58:42 +0800

    jpeg viewer OK

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,99 +1,297 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
+#include <QScrollArea>
 #include <QToolBar>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QScreen>
+#include <QImageReader>
+#include <QImageWriter>
+#include <QStandardPaths>
+#include <QByteArray>
+#include <QList>
+#include <QClipboard>
+#include <QMimeData>
+#include <QScrollBar>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    QMainWindow(parent),ui(new Ui::MainWindow),
+    mImageLabel(new QLabel),
+    mScrollArea(new QScrollArea),
+    mZoomFactor(1)
 {
     ui->setupUi(this);
+    mImageLabel->setBackgroundRole(QPalette::Base);
+    mImageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    mImageLabel->setScaledContents(true);
+
+    mScrollArea->setBackgroundRole(QPalette::Dark);
+    mScrollArea->setWidget(mImageLabel);
+    mScrollArea->setVisible(false);
+    setCentralWidget(mScrollArea);
+
     createMenuAndToolbar();
+    resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+bool MainWindow::loadFile(const QString &fileName)
+{
+    QImageReader reader(fileName);
+    reader.setAutoDetectImageFormat(true);
+    const QImage newImage = reader.read();
+    if (newImage.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot load %1: %2")
+                                 .arg(QDir::toNativeSeparators(fileName), reader.errorString()));
+        return false;
+    }
+
+    setImage(newImage);
+
+    setWindowFilePath(fileName);
+
+    const QString message = tr("Opened \"%1\", %2x%3, Depth: %4")
+        .arg(QDir::toNativeSeparators(fileName)).arg(mImage.width()).arg(mImage.height()).arg(mImage.depth());
+    statusBar()->showMessage(message);
+    return true;
+}
+void MainWindow::setImage(const QImage &newImage)
+{
+    mImage = newImage;
+    mImageLabel->setPixmap(QPixmap::fromImage(mImage));
+    mZoomFactor = 1.0;
+
+    mScrollArea->setVisible(true);
+    mFitToWindowAct->setEnabled(true);
+    updateActions();
+
+    if (!mFitToWindowAct->isChecked())
+        mImageLabel->adjustSize();
+}
+bool MainWindow::saveFile(const QString &fileName)
+{
+    QImageWriter writer(fileName);
+
+    if (!writer.write(mImage)) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot write %1: %2")
+                                 .arg(QDir::toNativeSeparators(fileName)), writer.errorString());
+        return false;
+    }
+    const QString message = tr("Wrote \"%1\"").arg(QDir::toNativeSeparators(fileName));
+    statusBar()->showMessage(message);
+    return true;
+}
+
+static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
+{
+    static bool firstDialog = true;
+
+    if (firstDialog) {
+        firstDialog = false;
+        const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+        dialog.setDirectory(picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+    }
+
+    QStringList mimeTypeFilters;
+    const  QList<QByteArray> supportedMimeTypes = acceptMode == QFileDialog::AcceptOpen
+        ? QImageReader::supportedMimeTypes() : QImageWriter::supportedMimeTypes();
+    foreach (const QByteArray &mimeTypeName, supportedMimeTypes)
+        mimeTypeFilters.append(mimeTypeName);
+    mimeTypeFilters.sort();
+    dialog.setMimeTypeFilters(mimeTypeFilters);
+    dialog.selectMimeTypeFilter("image/jpeg");
+    if (acceptMode == QFileDialog::AcceptSave)
+        dialog.setDefaultSuffix("jpg");
+}
+void MainWindow::onFileOpen()
+{
+    QFileDialog dialog(this, tr("Open File"));
+    initializeImageFileDialog(dialog, QFileDialog::AcceptOpen);
+
+    while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())) {}
+
+}
+void MainWindow::onFileSaveAs()
+{
+    QFileDialog dialog(this, tr("Save File As"));
+    initializeImageFileDialog(dialog, QFileDialog::AcceptSave);
+
+    while (dialog.exec() == QDialog::Accepted && !saveFile(dialog.selectedFiles().first())) {}
+
+}
+void MainWindow::onEditCopy()
+{
+#ifndef QT_NO_CLIPBOARD
+    QGuiApplication::clipboard()->setImage(mImage);
+#endif // !QT_NO_CLIPBOARD
+}
+#ifndef QT_NO_CLIPBOARD
+static QImage clipboardImage()
+{
+    if (const QMimeData *mimeData = QGuiApplication::clipboard()->mimeData()) {
+        if (mimeData->hasImage()) {
+            const QImage image = qvariant_cast<QImage>(mimeData->imageData());
+            if (!image.isNull())
+                return image;
+        }
+    }
+    return QImage();
+}
+#endif // !QT_NO_CLIPBOARD
+
+void MainWindow::onEditPaste()
+{
+#ifndef QT_NO_CLIPBOARD
+    const QImage newImage = clipboardImage();
+    if (newImage.isNull()) {
+        ui->statusBar->showMessage(tr("No image in clipboard"));
+    } else {
+        setImage(newImage);
+        setWindowFilePath(QString());
+        const QString message = tr("Obtained image from clipboard, %1x%2, Depth: %3")
+            .arg(newImage.width()).arg(newImage.height()).arg(newImage.depth());
+        ui->statusBar->showMessage(message);
+    }
+#endif // !QT_NO_CLIPBOARD
+}
+
+void MainWindow::onViewZoomin()
+{
+    scaleImage(1.25);
+}
+
+void MainWindow::onViewZoomout()
+{
+    scaleImage(0.8);
+}
+
+void MainWindow::onViewNormalSize()
+{
+    mImageLabel->adjustSize();
+    mZoomFactor = 1.0;
+}
+
+void MainWindow::onViewFitToWindow()
+{
+    bool fitToWindow = mFitToWindowAct->isChecked();
+    mScrollArea->setWidgetResizable(fitToWindow);
+    if (!fitToWindow)
+        onViewNormalSize();
+    updateActions();
+}
+
+void MainWindow::onHelpAbout()
+{
+    QMessageBox::about(this, tr("About Application"),
+             tr("The <b>Application</b>  Copyrights 2018."));
+}
+
 void MainWindow::closeEvent(QCloseEvent *event)
 {
     event->accept();
 }
 void MainWindow::createMenuAndToolbar()
 {
-    QMenu* fileMenu = ui->menuBar->addMenu(tr("&File"));
     QToolBar *fileToolBar = ui->mainToolBar;
+    QMenu* fileMenu = ui->menuBar->addMenu(tr("&File"));
     const QIcon openIcon = QIcon(":/images/open.png");
-    QAction* openAct = new QAction(openIcon, tr("&Open..."), this);
-    openAct->setStatusTip(tr("Open file."));
-    connect(openAct, SIGNAL(triggered()), SLOT(onFileOpen()));
+    QAction *openAct = fileMenu->addAction(openIcon, tr("&Open..."), this,
+                    SLOT(onFileOpen()), QKeySequence::Open);
+    openAct->setStatusTip(tr("Open an image file."));
     const QIcon saveIcon = QIcon(":/images/save.png");
-    QAction* saveAct = new QAction(saveIcon, tr("&Save..."), this);
-    openAct->setStatusTip(tr("Save file."));
-    connect(saveAct, SIGNAL(triggered()), SLOT(onFileSave()));
+    mSaveAsAct = fileMenu->addAction(saveIcon, tr("&Save..."), this,
+                                     SLOT(onFileSaveAs()));
+    mSaveAsAct->setStatusTip(tr("Save file."));
+    mSaveAsAct->setEnabled(false);
+    fileMenu->addSeparator();
     const QIcon closeIcon =  QIcon(":/images/exit.png");
-    QAction* closeAct = new QAction(closeIcon, tr("E&xit"), this);
+    QAction* closeAct = fileMenu->addAction(closeIcon,tr("E&xit"), this,
+            SLOT(close()), QKeySequence::Quit);
     closeAct->setStatusTip(tr("Exit this program."));
-    connect(closeAct, SIGNAL(triggered()), SLOT(close()));
-    fileMenu->addAction(openAct);
-    fileMenu->addAction(saveAct);
-    fileMenu->addSeparator();
-    fileMenu->addAction(closeAct);
     fileToolBar->addAction(openAct);
-    fileToolBar->addAction(saveAct);
+    fileToolBar->addAction(mSaveAsAct);
     fileMenu->addSeparator();
-
+    /*******************************************/
+    QMenu *editMenu = ui->menuBar->addMenu(tr("&Edit"));
+    const QIcon copyIcon =  QIcon(":/images/copy.png");
+    mCopyAct = editMenu->addAction(copyIcon, tr("&Copy"), this,
+                         SLOT(onEditCopy()),QKeySequence::Copy);
+    mCopyAct->setEnabled(false);
+    const QIcon pasteIcon =  QIcon(":/images/paste.png");
+    QAction *pasteAct = editMenu->addAction(pasteIcon, tr("&Paste"), this,
+                         SLOT(onEditPaste()), QKeySequence::Paste);
+    pasteAct->setStatusTip(tr("paste from clipboard."));
+    /*******************************************/
     QMenu* viewMenu = ui->menuBar->addMenu(tr("&View"));
     const QIcon zoominIcon =  QIcon(":/images/zoomin.png");
-    QAction* zoominAct = new QAction(zoominIcon, tr("Zoom &In"), this);
-    zoominAct->setStatusTip(tr("Magfying image."));
-    connect(zoominAct, SIGNAL(triggered()), SLOT(onViewZoomin()));
+    mZoomInAct = viewMenu->addAction(zoominIcon,tr("Zoom &In"), this,
+                                    SLOT(onViewZoomin()), QKeySequence::ZoomIn );
+    mZoomInAct->setEnabled(false);
+    mZoomInAct->setStatusTip(tr("Magfying image."));
 
     const QIcon zoomoutIcon = QIcon(":/images/zoomout.png");
-    QAction* zoomoutAct = new QAction(zoomoutIcon, tr("Zoom &Out"), this);
-    zoomoutAct->setStatusTip(tr("zoom in image."));
-    connect(zoomoutAct, SIGNAL(triggered()), SLOT(onViewZoomout()));
-    viewMenu->addAction(zoominAct);
-    viewMenu->addAction(zoomoutAct);
-    fileToolBar->addAction(zoominAct);
-    fileToolBar->addAction(zoomoutAct);
+    mZoomOutAct = viewMenu->addAction(zoomoutIcon,tr("Zoom &Out"), this,
+                     SLOT(onViewZoomout()), QKeySequence::ZoomOut );
+    mZoomOutAct->setStatusTip(tr("zoom out image."));
+    mZoomOutAct->setEnabled(false);
+    const QIcon normalSizeIcon = QIcon(":/images/normalsize.png");
+    mNormalSizeAct = viewMenu->addAction(normalSizeIcon, tr("Normal &Size"), this,
+                     SLOT(onViewNormalSize()), tr("Ctrl+S") );
+    viewMenu->addSeparator();
+    mNormalSizeAct->setStatusTip(tr("set normal size image."));
+    mNormalSizeAct->setEnabled(false);
+     const QIcon fitIcon = QIcon(":/images/fittowindow.png");
+    mFitToWindowAct = viewMenu->addAction(fitIcon, tr("Fit to Window"), this,
+                     SLOT(onViewFitToWindow()), tr("Ctrl+S") );
+    mFitToWindowAct->setStatusTip(tr("set normal size image."));
+    mFitToWindowAct->setEnabled(false);
+    mFitToWindowAct->setCheckable(true);
+    mFitToWindowAct->setShortcut(tr("Ctrl+F"));
+
+    fileToolBar->addAction(mZoomInAct);
+    fileToolBar->addAction(mZoomOutAct);
+    fileToolBar->addAction(mNormalSizeAct);
+    fileToolBar->addAction(mFitToWindowAct);
     fileToolBar->addSeparator() ;
 
     QMenu* helpMenu = ui->menuBar->addMenu(tr("&Help"));
     const QIcon aboutIcon = QIcon(":/images/about.png");
-    QAction* aboutAct = new QAction(aboutIcon, tr("A&bout"), this);
+    QAction* aboutAct = helpMenu->addAction(aboutIcon, tr("A&bout"), this, SLOT(onHelpAbout()));
     aboutAct->setStatusTip(tr("Copyrights."));
-    connect(aboutAct, SIGNAL(triggered()), SLOT(onHelpAbout()));
-     helpMenu->addAction(aboutAct);
-fileToolBar->addAction(aboutAct);
+    fileToolBar->addAction(aboutAct);
 
 
 }
-
-void MainWindow::onFileOpen()
+void MainWindow::updateActions()
 {
-    QString fileName = QFileDialog::getOpenFileName(this);
-    if (!fileName.isEmpty()) {
-
-    }
+    mSaveAsAct->setEnabled(!mImage.isNull());
+    mCopyAct->setEnabled(!mImage.isNull());
+    mZoomInAct->setEnabled(!mFitToWindowAct->isChecked());
+    mZoomOutAct->setEnabled(!mFitToWindowAct->isChecked());
+    mNormalSizeAct->setEnabled(!mFitToWindowAct->isChecked());
 }
-void MainWindow::onFileSave()
+void MainWindow::scaleImage(double factor)
 {
+    Q_ASSERT(mImageLabel->pixmap());
+    mZoomFactor *= factor;
+    mImageLabel->resize(mZoomFactor * mImageLabel->pixmap()->size());
 
+    adjustScrollBar(mScrollArea->horizontalScrollBar(), factor);
+    adjustScrollBar(mScrollArea->verticalScrollBar(), factor);
+
+    mZoomInAct->setEnabled(mZoomFactor < 10.0);
+    mZoomOutAct->setEnabled(mZoomFactor > 0.1);
 }
-
-void MainWindow::onViewZoomin()
+void MainWindow::adjustScrollBar(QScrollBar *scrollBar, double factor)
 {
-
+    scrollBar->setValue(int(factor * scrollBar->value()
+                            + ((factor - 1) * scrollBar->pageStep()/2)));
 }
 
-void MainWindow::onViewZoomout()
-{
-
-}
-void MainWindow::onHelpAbout()
-{
-    QMessageBox::about(this, tr("About Application"),
-             tr("The <b>Application</b>  Copyrights 2018."));
-}
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 #include <QCloseEvent>
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
 
 namespace Ui {
 class MainWindow;
@@ -15,20 +20,41 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void createMenuAndToolbar();
+    bool loadFile(const QString &);
 
 protected:
     void closeEvent(QCloseEvent *event) override;
 
 private slots:
      void onFileOpen();
-     void onFileSave();
+     void onFileSaveAs();
+     void onEditCopy();
+     void onEditPaste();
      void onViewZoomin();
      void onViewZoomout();
+     void onViewNormalSize();
+     void onViewFitToWindow();
      void onHelpAbout();
-
 private:
+    void createMenuAndToolbar();
+    void updateActions();
+    bool saveFile(const QString &fileName);
+    void setImage(const QImage &newImage);
+    void scaleImage(double factor);
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+
     Ui::MainWindow *ui;
+
+    QImage mImage;
+    QLabel *mImageLabel;
+    QScrollArea *mScrollArea;
+    double mZoomFactor;
+    QAction *mSaveAsAct;
+    QAction *mCopyAct;
+    QAction *mZoomInAct;
+    QAction *mZoomOutAct;
+    QAction *mNormalSizeAct;
+    QAction *mFitToWindowAct;
 };
 
 #endif // MAINWINDOW_H
